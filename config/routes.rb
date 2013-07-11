2c1486d2eefa49d4081be3810ed10057fec452c1
Chris Heydt <chrisheydt@gmail.com>
2013-07-11 13:09:29 -0400

    Stations index is default route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Citistats::Application.routes.draw do
+  root to: 'stations#index'
+  
   resources :stations, only: [:index] do
     resources :snapshots, controller: 'station_snapshots', only: [:index]
   end
